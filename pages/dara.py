6e59f51ab51275b763e59ca38088199865315a8f
2441018-js <2441018@mokwon.ac.kr>
2025-09-25 07:45:14 +0000

    대강 오류 수정

--- pages/dara.py
+++ pages/dara.py
@@ -1,21 +1,22 @@
-# 한글 폰트 설정
-# 한글 폰트 설정
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-import os
-# 실제 폰트 경로 및 이름으로 수정
-font_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../fonst/NanumGothic-Regular.ttf'))
-if os.path.exists(font_path):
-	fontprop = fm.FontProperties(fname=font_path)
-	plt.rcParams['font.family'] = fontprop.get_name()
-	plt.rcParams['axes.unicode_minus'] = False
-else:
-	st.warning(f'폰트 파일을 찾을 수 없습니다: {font_path}')
-st.markdown('<style>div[data-testid="stMarkdownContainer"]{ font-family: NanumGothic, sans-serif; }</style>', unsafe_allow_html=True)
 
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import os
+
+# 실제 폰트 경로 및 이름으로 수정
+font_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../fonst/NanumGothic-Regular.ttf'))
+if os.path.exists(font_path):
+    fontprop = fm.FontProperties(fname=font_path)
+    plt.rcParams['font.family'] = fontprop.get_name()
+    plt.rcParams['axes.unicode_minus'] = False
+else:
+    st.warning(f'폰트 파일을 찾을 수 없습니다: {font_path}')
+
+# Streamlit 스타일(한글 폰트 적용)
+st.markdown('<style>div[data-testid="stMarkdownContainer"]{ font-family: NanumGothic, sans-serif; }</style>', unsafe_allow_html=True)
 
 st.title('간단한 데이터 시각화')
 
